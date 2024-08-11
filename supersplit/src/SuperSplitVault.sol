e7ad2e1f08f87a0ad0f304d9e6f45cf1fe1f0afe
Juan Palomino <jpalomino09@gmail.com>
2024-08-10 19:29:53 -0700

    compiling contract

--- supersplit/src/SuperSplitVault.sol
+++ supersplit/src/SuperSplitVault.sol
@@ -8,10 +8,11 @@ import "@openzeppelin/contracts/utils/math/Math.sol";
 import "@openzeppelin/contracts/interfaces/IERC4626.sol";
 import "./IYieldStrategy.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 
 // Interface for the yield strategy
 
-contract SuperSplitVault is ERC20, IERC4626, Ownable {
+contract SuperSplitVault is ERC20, ERC4626, Ownable {
     using SafeERC20 for IERC20;
     using Math for uint256;
 
@@ -27,41 +28,54 @@ contract SuperSplitVault is ERC20, IERC4626, Ownable {
     mapping(address => DebtEntry[]) public memberDebts;
     address[] private _members;
 
-    constructor(IERC20 asset_, IYieldStrategy yieldStrategy_) ERC20("SuperSplitVault Shares", "SSVS") {
-        (bool success, uint8 assetDecimals) = _tryGetAssetDecimals(asset_);
+    constructor(
+        string memory name,
+        string memory symbol,
+        IERC20 asset_,
+        IYieldStrategy yieldStrategy_
+    ) ERC20(name, symbol) ERC4626(asset_) Ownable(msg.sender) {
+        (bool success, uint8 assetDecimals) = _getAssetDecimalsInternal(asset_);
         _underlyingDecimals = success ? assetDecimals : 18;
         _asset = asset_;
         yieldStrategy = yieldStrategy_;
     }
 
-    // Deposit assets and mint shares
-    function deposit(uint256 assets, address receiver) public virtual returns (uint256) {
-        uint256 shares = previewDeposit(assets);
+    function decimals() public view virtual override(ERC20, ERC4626) returns (uint8) {
+        return _underlyingDecimals;
+    }
+
+    // Custom internal function to avoid conflicts
+    function _getAssetDecimalsInternal(IERC20 asset_) private view returns (bool, uint8) {
+        (bool success, bytes memory encodedDecimals) = address(asset_).staticcall(
+            abi.encodeCall(IERC20Metadata.decimals, ())
+        );
+        if (success && encodedDecimals.length >= 32) {
+            uint256 returnedDecimals = abi.decode(encodedDecimals, (uint256));
+            if (returnedDecimals <= type(uint8).max) {
+                return (true, uint8(returnedDecimals));
+            }
+        }
+        return (false, 0);
+    }
+
+    function deposit(uint256 assets, address receiver) public override(ERC4626) returns (uint256 shares) {
+        shares = previewDeposit(assets);  // Correctly inherits from ERC4626
         _deposit(_msgSender(), receiver, assets, shares);
-        _addMember(receiver);
-        return shares;
     }
 
-    // Mint shares by depositing assets
-    function mint(uint256 shares, address receiver) public virtual returns (uint256) {
-        uint256 assets = previewMint(shares);
+    function mint(uint256 shares, address receiver) public override(ERC4626) returns (uint256 assets) {
+        assets = previewMint(shares);  // Correctly inherits from ERC4626
         _deposit(_msgSender(), receiver, assets, shares);
-        _addMember(receiver);
-        return assets;
     }
 
-    // Withdraw assets by burning shares
-    function withdraw(uint256 assets, address receiver, address owner) public virtual returns (uint256) {
-        uint256 shares = previewWithdraw(assets);
+    function withdraw(uint256 assets, address receiver, address owner) public override(ERC4626) returns (uint256 shares) {
+        shares = previewWithdraw(assets);  // Correctly inherits from ERC4626
         _withdraw(_msgSender(), receiver, owner, assets, shares);
-        return shares;
     }
 
-    // Redeem shares for assets
-    function redeem(uint256 shares, address receiver, address owner) public virtual returns (uint256) {
-        uint256 assets = previewRedeem(shares);
+    function redeem(uint256 shares, address receiver, address owner) public override(ERC4626) returns (uint256 assets) {
+        assets = previewRedeem(shares);  // Correctly inherits from ERC4626
         _withdraw(_msgSender(), receiver, owner, assets, shares);
-        return assets;
     }
 
     // Issue a new debt entry for a member
@@ -96,7 +110,7 @@ contract SuperSplitVault is ERC20, IERC4626, Ownable {
 
     // Distribute excess liquidity after settlement
     function distributeExcessLiquidity() external onlyOwner {
-        uint256 assetsTotal = totalAssets();
+     
         for (uint256 i = 0; i < _members.length; i++) {
             address member = accountAtIndex(i);
             uint256 positiveBalance = balanceOf(member);
@@ -108,15 +122,15 @@ contract SuperSplitVault is ERC20, IERC4626, Ownable {
     }
 
     // Helpers for conversions and total assets
-    function convertToShares(uint256 assets) public view virtual returns (uint256) {
+    function convertToShares(uint256 assets) public view virtual override(ERC4626) returns (uint256) {
         return assets.mulDiv(totalSupply() + 10 ** _underlyingDecimals, totalAssets() + 1, Math.Rounding.Floor);
     }
 
-    function convertToAssets(uint256 shares) public view virtual returns (uint256) {
+    function convertToAssets(uint256 shares) public view virtual override(ERC4626) returns (uint256) {
         return shares.mulDiv(totalAssets() + 1, totalSupply() + 10 ** _underlyingDecimals, Math.Rounding.Floor);
     }
 
-    function totalAssets() public view virtual returns (uint256) {
+    function totalAssets() public view virtual override(ERC4626) returns (uint256) {
         return yieldStrategy.totalAssets() + _asset.balanceOf(address(this));
     }
 
@@ -124,27 +138,15 @@ contract SuperSplitVault is ERC20, IERC4626, Ownable {
         return _members[index];
     }
 
-    function _tryGetAssetDecimals(IERC20 asset_) private view returns (bool, uint8) {
-        (bool success, bytes memory encodedDecimals) = address(asset_).staticcall(
-            abi.encodeCall(IERC20Metadata.decimals, ())
-        );
-        if (success && encodedDecimals.length >= 32) {
-            uint256 returnedDecimals = abi.decode(encodedDecimals, (uint256));
-            if (returnedDecimals <= type(uint8).max) {
-                return (true, uint8(returnedDecimals));
-            }
-        }
-        return (false, 0);
-    }
 
-    function _deposit(address caller, address receiver, uint256 assets, uint256 shares) internal virtual {
+    function _deposit(address caller, address receiver, uint256 assets, uint256 shares) internal virtual override(ERC4626) {
         SafeERC20.safeTransferFrom(_asset, caller, address(this), assets);
         yieldStrategy.deposit(assets); // Deposit into the yield strategy
         _mint(receiver, shares);
         emit Deposit(caller, receiver, assets, shares);
     }
 
-    function _withdraw(address caller, address receiver, address owner, uint256 assets, uint256 shares) internal virtual {
+    function _withdraw(address caller, address receiver, address owner, uint256 assets, uint256 shares) internal virtual override(ERC4626) {
         if (caller != owner) {
             _spendAllowance(owner, caller, shares);
         }
